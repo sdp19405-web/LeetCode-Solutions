db5401d89eb602a3d53576b491f87d522f02b983
Shreyash Patil <sdp19405@gmail.com>
2026-08-23 05:03:28 +0530

    Optimal solution

---
+++ LinkedList.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public static void main (String args[]){
+        LinkedList ll=new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+    }
+}
