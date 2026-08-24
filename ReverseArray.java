d67755c857bdfc0d6c0e9e80b2112491865c76e3
Shreyash Patil <sdp19405@gmail.com>
2026-08-24 12:42:27 +0530

    two pointer approach

---
+++ ReverseArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ReverseArray{
+    public static void reverse(int a[]){
+        int start=0;
+        int end=a.length-1;
+        while(start<end){
+            int temp=a[start];
+            a[start]=a[end];
+            a[end]=temp;
+            start++;
+            end--;
+        }
+        
+    }
+    public static void main (String args[]){
+        int a[]={1,2,3,4,5};
+        reverse(a);
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
